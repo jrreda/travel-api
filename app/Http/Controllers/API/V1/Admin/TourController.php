29bbc68b720f177122fc8e8c6b533e0e60737714
Mahmoud Reda <mahmoudreda457@gmail.com>
2024-09-15 11:55:07 +0300

    Larastan

--- app/Http/Controllers/API/V1/Admin/TourController.php
+++ app/Http/Controllers/API/V1/Admin/TourController.php
@@ -31,8 +31,6 @@ class TourController extends Controller
      */
     public function store(Travel $travel, TourRequest $request)
     {
-        dd(auth()->user()->roles);
-
         $tour = $travel->tours()->create($request->validated());
 
         return new TourResource($tour);
